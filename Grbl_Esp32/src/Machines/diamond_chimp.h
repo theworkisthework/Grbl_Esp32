036cf27ad86f8add70e24953e62c6dff8796c25f
Mark Benson <mark@sub-design.co.uk>
2021-04-07 17:41:17 +0100

    Add machine config file for ESP32_Plotter_Controller RevA aka Diamond Chimp

---
+++ Grbl_Esp32/src/Machines/diamond_chimp.h
@@ -0,0 +1,59 @@
+#pragma once
+// clang-format off
+
+/*
+    diamond_chimp.h
+    
+    Pin assignments for the ESP32 Plotter Controller RevA (Diamond Chimp)
+    https://github.com/MarkJB/ESP32_Plotter_Controller
+
+    2021    - Mark Benson
+    
+    Grbl_ESP32 is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Grbl is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Grbl_ESP32.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+// Select a version to match your PCB
+
+#define MACHINE_NAME    "ESP32_Plotter_Controller_RevA_DIAMOND_CHIMP"
+#define X_LIMIT_PIN     GPIO_NUM_34
+
+#define TRINAMIC_RUN_MODE           TrinamicMode :: CoolStep
+#define TRINAMIC_HOMING_MODE        TrinamicMode :: CoolStep
+
+#define X_STEP_PIN              GPIO_NUM_25
+#define X_DIRECTION_PIN         GPIO_NUM_26
+#define X_TRINAMIC_DRIVER       2130        // Which Driver Type?
+#define X_CS_PIN                GPIO_NUM_17  //chip select
+#define X_RSENSE                TMC2130_RSENSE_DEFAULT
+
+#define Y_STEP_PIN              GPIO_NUM_32
+#define Y_DIRECTION_PIN         GPIO_NUM_26
+#define Y_TRINAMIC_DRIVER       2130        // Which Driver Type?
+#define Y_CS_PIN                GPIO_NUM_16  //chip select
+#define Y_RSENSE                TMC2130_RSENSE_DEFAULT
+
+// OK to comment out to use pin for other features
+#define STEPPERS_DISABLE_PIN GPIO_NUM_2
+
+
+// Define one of these 2 options for spindle or servo
+#define Z_SERVO_PIN                   GPIO_NUM_27 // comment this out if PWM spindle/laser control.
+#define DEFAULT_Z_MAX_TRAVEL          5.0   // Range of travel is 5mm
+#define DEFAULT_Z_HOMING_MPOS         5.0   // MPos will be set to 5mm after homing
+#define Z_SERVO_CAL_MIN               1.0   // calibration factor for the minimum PWM duty
+#define Z_SERVO_CAL_MAX               1.0   // calibration factor for the maximum PWM duty
+
+// #define X_LIMIT_PIN          See version section at beginning of file
+#define Y_LIMIT_PIN             GPIO_NUM_35
